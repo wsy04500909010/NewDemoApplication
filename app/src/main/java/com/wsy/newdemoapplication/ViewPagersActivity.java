99a2f1cc37d52b8e3d72163e2103be5265f8cefb
WSY <niaoniaov@126.com>
2018-12-24 15:43:16 +0800

    add pageradapter

--- app/src/main/java/com/wsy/newdemoapplication/ViewPagersActivity.java
+++ app/src/main/java/com/wsy/newdemoapplication/ViewPagersActivity.java
@@ -2,7 +2,10 @@ package com.wsy.newdemoapplication;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 
+import com.wsy.newdemoapplication.adapter.PagerOnlyAdapter;
 import com.wsy.newdemoapplication.adapter.ViewPagerAdapter;
 import com.wsy.newdemoapplication.base.BaseActivity;
 import com.wsy.newdemoapplication.fragment.MyFragment;
@@ -19,12 +22,34 @@ public class ViewPagersActivity extends BaseActivity {
 
     @BindView(R.id.viewpager)
     ViewPager viewPager;
+    @BindView(R.id.btn_viewpager)
+    Button btn_viewpager;
+    @BindView(R.id.btn_fragmentviewpager)
+    Button btn_fragmentviewpager;
 
     ViewPagerAdapter viewPagerAdapter;
+    PagerOnlyAdapter pagerOnlyAdapter;
     List<Fragment> fragments;
 
     @Override
     protected void init() {
+
+        initDefault();
+
+        btn_viewpager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initDefault();
+            }
+        });
+
+        btn_fragmentviewpager.setOnClickListener((v) -> {
+            initPageOnly();
+        });
+
+    }
+
+    private void initDefault() {
         fragments = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
 
@@ -34,8 +59,18 @@ public class ViewPagersActivity extends BaseActivity {
 
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
         viewPager.setAdapter(viewPagerAdapter);
+    }
 
+    private void initPageOnly() {
 
+        List<String> datas = new ArrayList<>();
+        datas.add("1");
+        datas.add("2");
+        datas.add("3");
+        datas.add("4");
+        datas.add("5");
+        pagerOnlyAdapter = new PagerOnlyAdapter(datas,ViewPagersActivity.this);
+        viewPager.setAdapter(pagerOnlyAdapter);
     }
 
     @Override
--- app/src/main/java/com/wsy/newdemoapplication/adapter/PagerOnlyAdapter.java
+++ app/src/main/java/com/wsy/newdemoapplication/adapter/PagerOnlyAdapter.java
@@ -1,21 +1,59 @@
 package com.wsy.newdemoapplication.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wsy.newdemoapplication.R;
+
+import java.util.List;
 
 /**
  * Created by WangSiYe on 2018/12/24.
  */
 public class PagerOnlyAdapter extends PagerAdapter {
-    
+
+    List<String> titles;
+    Context context;
+
+
+    public PagerOnlyAdapter(List<String> titles, Context context) {
+        this.titles = titles;
+        this.context = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return titles.size();
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return false;
+        return view == object;
+    }
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_viewpager, null);
+        //在这里可以做相应的操作
+        TextView tv = view.findViewById(R.id.tv);
+        //数据填充
+        tv.setText(titles.get(position));
+
+        container.addView(view);    //这一步很重要
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
     }
 }
